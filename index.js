4db3bdd0038f32f10f6137360efb77a76dbba79a
Savannah C <sgcorrell@gmail.com>
2019-10-24 15:30:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function createEmployeeRecord(array) {
-
+  const employeeRecord = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  return employeeRecord;
 }
